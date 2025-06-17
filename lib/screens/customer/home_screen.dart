da201d024e53a550986b1625acb86cb0a1aa95f6
Afsal <euphcudd.dev@gmail.com>
2025-06-17 09:15:43 -0700

    make three screens home cart orders

--- lib/screens/customer/home_screen.dart
+++ lib/screens/customer/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class Home_Screen extends StatelessWidget {
-  const Home_Screen({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(color: Colors.white);
   }
 }
